f7851850e0bf860298d64d5eff4ef161851b8f87
Oleksandr Savchenko <alexandr.savchenko04@gmail.com>
2026-04-24 03:43:10 +0300

    feat: add user entity

--- src/main/java/com/sample/auth/module/User.java
+++ src/main/java/com/sample/auth/module/User.java
@@ -2,11 +2,13 @@ package com.sample.auth.module;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import org.hibernate.annotations.UuidGenerator;
 
 import java.util.UUID;
 
 @Entity
+@Table(name = "users")
 public record User(
         @Id
         @UuidGenerator
