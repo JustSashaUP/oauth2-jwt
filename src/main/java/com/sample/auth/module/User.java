c868968ca147b31f285e9930ee8973dfde6314b5
Oleksandr Savchenko <alexandr.savchenko04@gmail.com>
2026-04-26 07:50:46 +0300

    feat: change a record to the class: - add new fields; - add necessary constructions and methods.

--- src/main/java/com/sample/auth/module/User.java
+++ src/main/java/com/sample/auth/module/User.java
@@ -9,11 +9,55 @@ import java.util.UUID;
 
 @Entity
 @Table(name = "users")
-public record User(
+public class User {
         @Id
         @UuidGenerator
-        UUID id,
-        String name,
-        String email
-) {
+        private UUID id;
+        private String email;
+        private String name;
+        private String profilePicture;
+
+        private Provider provider;
+        private String providerId;
+
+        public User() {}
+
+        public User(String email, String name, String profilePicture, Provider provider, String providerId) {
+                this.email = email;
+                this.name = name;
+                this.profilePicture = profilePicture;
+                this.provider = provider;
+                this.providerId = providerId;
+        }
+
+        public UUID getId() {
+                return id;
+        }
+
+        public void setId(UUID id) {
+                this.id = id;
+        }
+
+        public void setName(String name) {
+                this.name = name;
+        }
+
+        public String getEmail() {
+                return email;
+        }
+
+        public void setProfilePicture(String profilePicture) {
+                this.profilePicture = profilePicture;
+        }
+
+        @Override
+        public String toString() {
+                return "User{" +
+                        "email='" + email + '\'' +
+                        ", name='" + name + '\'' +
+                        ", profilePicture='" + profilePicture + '\'' +
+                        ", provider=" + provider +
+                        ", providerId='" + providerId + '\'' +
+                        '}';
+        }
 }
