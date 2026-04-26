a66b803ea9b7807984a9af553604759cead29a01
Oleksandr Savchenko <alexandr.savchenko04@gmail.com>
2026-04-26 07:48:43 +0300

    fix: add variants of the @Components annotation - add @Service above the UserService class; - add @Repository above the UserRepository interface.

--- src/main/java/com/sample/auth/repository/UserRepository.java
+++ src/main/java/com/sample/auth/repository/UserRepository.java
@@ -2,10 +2,12 @@ package com.sample.auth.repository;
 
 import com.sample.auth.module.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
 }
--- src/main/java/com/sample/auth/service/UserService.java
+++ src/main/java/com/sample/auth/service/UserService.java
@@ -4,7 +4,9 @@ import com.sample.auth.module.User;
 import com.sample.auth.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService {
     private final UserRepository userRepository;
 
@@ -15,7 +17,7 @@ public class UserService {
     @Transactional
     public User findOrCreateUser(User user) {
         try {
-            return userRepository.findByEmail(user.email())
+            return userRepository.findByEmail(user.getEmail())
                     .orElseGet(() -> userRepository.save(user));
         } catch(DataIntegrityViolationException ex) {
             throw new RuntimeException("Data constrain violation while save user.");
