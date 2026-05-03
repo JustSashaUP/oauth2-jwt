51969d14246aa09e5ab8eebdbb354078c0fa0ce5
Oleksandr Savchenko <alexandr.savchenko04@gmail.com>
2026-05-03 16:00:57 +0300

    feat: add renderProfile function to display data about the user properly as a picture, name and email.

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -5,6 +5,22 @@ window.login = function () {
         window.location.origin + "/oauth2/authorization/google";
 };
 
+function renderProfile(profile) {
+    const profilePicture = document.createElement("img");
+    const name = document.createElement("div");
+    const email = document.createElement("div");
+
+    profilePicture.src = profile.picture;
+    profilePicture.alt = profile.name ? `${profile.name} picture` : "Profile profilePicture";
+    profilePicture.width = 96;
+    profilePicture.height = 96;
+
+    name.textContent = `${profile.name ?? ""}`;
+    email.textContent = `${profile.email ?? ""}`;
+
+    output.replaceChildren(profilePicture, name, email);
+}
+
 window.loadProfile = async function () {
     const resp = await fetch(window.location.origin + "/api/profile", {
         credentials: "include"
@@ -14,8 +30,8 @@ window.loadProfile = async function () {
         output.textContent = "Unauthorized: no token or invalid!"
     }
 
-    const data = await resp.text();
-    output.textContent = data;
+    const data = await resp.json();
+    renderProfile(data);
 };
 
 window.logout = function () {
